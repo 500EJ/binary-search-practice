2275733257c0b8ff78be7128ebcbc0eb562e8ad7
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2023-02-12 13:45:18 -0500

    Implement binarySearch

--- binary-search.ts
+++ binary-search.ts
@@ -3,20 +3,17 @@ export function linearSearch(arr: number[], target: number): number {
 }
 
 export function binarySearch(arr: number[], target: number): number {
-  // Set integers pointing to the high and low range of possible indices
-
-  // While high and low indices do not overlap...
-
-    // Find the midpoint between high and low indices
-
-    // Compare the target value to the midpoint value
-
-    // If the target equals the midpoint...
-      // Return the midpoint index
-    // If the target is higher than the midpoint...
-      // Move the low pointer to midpoint + 1
-    // If the target is less than the midpoint...
-      // Move the high pointer to midpoint - 1
-
-  // Return -1 if the loop exits with overlapping pointers
+  let [low, high] = [0, arr.length - 1];
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const midEl = arr[mid];
+    if (arr[mid] === target) {
+      return mid;
+    } else if (midEl != null && midEl < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
 }
