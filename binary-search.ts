188d3b630b9f73535348a6a0b3872b4fd9df119b
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2023-02-15 17:28:56 -0500

    Refactor linearSearch

--- binary-search.ts
+++ binary-search.ts
@@ -1,5 +1,8 @@
 export function linearSearch(arr: number[], target: number): number {
-  return arr.findIndex(n => n === target);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+  return -1;
 }
 
 export function binarySearch(arr: number[], target: number): number {
